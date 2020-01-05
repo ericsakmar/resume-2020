86c35ccf60f8c1a0dfcb3d1b23073d09ce5fa5d5
Eric Sakmar <eric.sakmar@gmail.com>
2020-01-05 14:15:57 -0500

    adds icon, updates colors

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
         background_color: `#171412`,
         theme_color: `#171412`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/resume-icon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-transformer-json`,
